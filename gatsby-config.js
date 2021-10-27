5728b64739db5d81568a9fecbbc94e7ad08596f4
Operel <oriperelman@gmail.com>
2021-10-27 12:24:25 +0300

    Install ackee tracker plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,21 @@ module.exports = {
     siteUrl: "https://www.yourdomain.tld",
     title: "ackee tracker issue",
   },
-  plugins: [],
+  plugins: [
+    {
+      resolve: 'gatsby-plugin-ackee-tracker',
+      options: {
+        // Domain ID found when adding a domain in the admin panel.
+        domainId: process.env.ACKEE_DOMAIN_ID,
+        // URL to Server eg: "https://analytics.test.com".
+        server: process.env.ACKEE_SERVER,
+        // Disabled analytic tracking when running locally
+        ignoreLocalhost: false,
+        // Enable or disable the tracking of your own visits (as identified by your login to the Ackee dashboard).
+        ignoreOwnVisits: false,
+        // If enabled it will collect info on OS, BrowserInfo, Device  & ScreenSize
+        detailed: true,
+      },
+    }
+  ],
 };
